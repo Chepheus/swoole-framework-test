c091d1d3bbfa7dceb503468cc22b63efadca8c97
anton-zamniborshch <anton.zamniborshch@boosta.co>
2021-07-26 14:23:17 +0300

    Add 'hello world' for symfony-swoole

--- code/symfony-swoole/config/bundles.php
+++ code/symfony-swoole/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     K911\Swoole\Bridge\Symfony\Bundle\SwooleBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
